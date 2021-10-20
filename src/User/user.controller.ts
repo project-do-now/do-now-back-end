884402b5370cd943a3bd22f06565d17398046799
kyumin0411 <rbalsgood@gmail.com>
2021-10-20 22:06:58 +0900

    [Init] User 폴더 생성

--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Delete, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './user.service';
 
 @Controller()
 export class AppController {
