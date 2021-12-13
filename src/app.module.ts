e5b96b2e0f42beb7ec283605696d4b569e20a5f9
kyumin0411 <85981698+kyumin0411@users.noreply.github.com>
2021-12-13 21:16:55 +0900

    [dev] env 파일 생성 (#9)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,24 @@ import { DiaryModule } from 'src/Diary/diary.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User, Schedule, Diary } from 'src/entity';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
     ScheduleModule,
     DiaryModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'alticast#',
-      database: 'Donow',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User, Schedule, Diary],
       migrations: ['migrations/*{.ts,.js}'],
       synchronize: false,
